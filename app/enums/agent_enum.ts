5ed14483df396a7abbfa3abe3a59a1c3a560a7e3
Ghulam-Mustafa <themustafadeveloper@gmail.com>
2025-11-25 17:58:13 +0500

    udpate list

--- app/enums/agent_enum.ts
+++ app/enums/agent_enum.ts
@@ -1,8 +1,4 @@
 export const modelAgents = [
-  'gpt-5-mini',
-  'gpt-4.1',
-  'gemini-2.5-flash',
-  'gemini-2.5-flash-lite',
   'anthropic.claude-3-5-sonnet-20241022-v2:0',
   'anthropic.claude-3-5-sonnet-20241022-v1:0',
   'anthropic.claude-3-opus-20240229-v1:0',
@@ -13,10 +9,6 @@ export const modelAgents = [
 ] as const
 
 export const agentModelKeys = {
-  GPT_5_MINI: 'gpt-5-mini',
-  GPT_4_1: 'gpt-4.1',
-  GEMINI_2_5_FLASH: 'gemini-2.5-flash',
-  GEMINI_2_5_FLASH_LITE: 'gemini-2.5-flash-lite',
   CLAUDE_3_5_SONNET_V2: 'anthropic.claude-3-5-sonnet-20241022-v2:0',
   CLAUDE_3_5_SONNET_V1: 'anthropic.claude-3-5-sonnet-20241022-v1:0',
   CLAUDE_3_OPUS: 'anthropic.claude-3-opus-20240229-v1:0',
