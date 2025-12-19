b2d3c2220cabe02d267e6203c5f62cb7c029c529
Ghulam-Mustafa <themustafadeveloper@gmail.com>
2025-12-19 15:39:35 +0500

    update keys

--- app/enums/evaluation_prompt_enum.ts
+++ app/enums/evaluation_prompt_enum.ts
@@ -1,5 +1,5 @@
 // Evaluation Prompt Keys Enum
 export const EvaluationPromptKeys = {
-  currentSession: 'CURRENT_NOTE',
+  currentSession: 'CURRENT_SESSION',
   previousSession: 'PREVIOUS_SESSION',
 }
