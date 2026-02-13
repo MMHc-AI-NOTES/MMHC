19facfdf7c1e2b6f283385e6f19bb5967cce0456
Ghulam-Mustafa <themustafadeveloper@gmail.com>
2026-02-13 15:30:38 +0500

    add redis

--- start/env.ts
+++ start/env.ts
@@ -50,4 +50,13 @@ export default await Env.create(new URL('../', import.meta.url), {
   AWS_SECRET_ACCESS_KEY: Env.schema.string.optional(),
   BEDROCK_MAX_TOKENS: Env.schema.number.optional(),
   BEDROCK_ANTHROPIC_VERSION: Env.schema.string.optional(),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring Redis (BullMQ)
+  |----------------------------------------------------------
+  */
+  REDIS_HOST: Env.schema.string({ format: 'host' }),
+  REDIS_PORT: Env.schema.number.optional(),
+  REDIS_PASSWORD: Env.schema.string.optional(),
 })
