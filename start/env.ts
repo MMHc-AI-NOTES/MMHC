6681a703cf398c8c56e7bc7a5f7bf38389f2ce40
Tariq Sajid <mtariqsajid@gmail.com>
2026-02-16 19:30:45 +0500

    update the github actions

--- start/env.ts
+++ start/env.ts
@@ -59,4 +59,6 @@ export default await Env.create(new URL('../', import.meta.url), {
   REDIS_HOST: Env.schema.string({ format: 'host' }),
   REDIS_PORT: Env.schema.number.optional(),
   REDIS_PASSWORD: Env.schema.string.optional(),
+  REDIS_DB: Env.schema.number.optional(),
+  REDIS_PREFIX: Env.schema.string.optional(),
 })
