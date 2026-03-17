259a96deccea6675cdd060ec0e9b040274e686ff
aqibawan12 <awanaqib826@gmail.com>
2026-03-17 15:13:13 +0500

    add dispatch email queue and worker implementation

---
+++ commands/dispatch_email_worker.ts
@@ -0,0 +1,46 @@
+import { BaseCommand, flags } from '@adonisjs/core/ace'
+import type { CommandOptions } from '@adonisjs/core/types/ace'
+import {
+  startDispatchEmailWorker,
+  stopDispatchEmailWorker,
+} from '#jobs/workers/dispatch_email_worker'
+
+export default class DispatchEmailWorker extends BaseCommand {
+  static commandName = 'dispatch_email_worker'
+  static description = 'Start or stop the dispatch email queue worker'
+
+  static options: CommandOptions = {
+    startApp: true,
+  }
+
+  @flags.boolean({ alias: 's', description: 'Stop the worker' })
+  declare stop: boolean
+
+  async run() {
+    if (this.stop) {
+      this.logger.info('Stopping dispatch email worker...')
+      await stopDispatchEmailWorker()
+      this.logger.success('Dispatch email worker stopped successfully')
+      return
+    }
+
+    this.logger.info('Starting dispatch email worker...')
+    startDispatchEmailWorker()
+    this.logger.success('Dispatch email worker started successfully')
+    this.logger.info('Press Ctrl+C to stop the worker')
+
+    process.on('SIGINT', async () => {
+      this.logger.info('\nStopping dispatch email worker...')
+      await stopDispatchEmailWorker()
+      process.exit(0)
+    })
+
+    process.on('SIGTERM', async () => {
+      this.logger.info('\nStopping dispatch email worker...')
+      await stopDispatchEmailWorker()
+      process.exit(0)
+    })
+
+    await new Promise(() => {})
+  }
+}
