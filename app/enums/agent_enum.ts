99b5115a86c5b140b2874be23d86c4e442d983ef
Ghulam-Mustafa <themustafadeveloper@gmail.com>
2026-04-02 14:29:07 +0500

    add model

--- app/enums/agent_enum.ts
+++ app/enums/agent_enum.ts
@@ -9,6 +9,7 @@ export const modelAgents = [
   'us.amazon.nova-premier-v1:0',
   'arn:aws:bedrock:us-east-1:199990519622:custom-model-deployment/y5k4mxdxqxbx',
   'arn:aws:bedrock:us-east-1:199990519622:custom-model-deployment/sugrzn0ke8p6',
+  'arn:aws:bedrock:us-east-1:199990519622:custom-model-deployment/cxwsnnx1qsw8',
 ] as const
 
 export const agentModelKeys = {
@@ -24,9 +25,12 @@ export const agentModelKeys = {
     'arn:aws:bedrock:us-east-1:199990519622:custom-model-deployment/y5k4mxdxqxbx',
   CUSTOM_DEPLOYMENT_V2_28032026:
     'arn:aws:bedrock:us-east-1:199990519622:custom-model-deployment/sugrzn0ke8p6',
+  CUSTOM_DEPLOYMENT_V3_02042026:
+    'arn:aws:bedrock:us-east-1:199990519622:custom-model-deployment/cxwsnnx1qsw8',
 } as const
 
 export const customChatDeploymentModels = [
   agentModelKeys.CUSTOM_DEPLOYMENT_V1_15032026,
   agentModelKeys.CUSTOM_DEPLOYMENT_V2_28032026,
+  agentModelKeys.CUSTOM_DEPLOYMENT_V3_02042026,
 ] as const
