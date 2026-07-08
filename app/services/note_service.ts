43c0ecf68cc7d4c71fbf9bccc3a6c0c92c6a6c53
saif-9-coder <saif-9-coder@users.noreply.github.com>
2026-07-08 12:56:49 +0000

    Prettified Code!

--- app/services/note_service.ts
+++ app/services/note_service.ts
@@ -383,8 +383,7 @@ export const getNoteWithChats = async (noteId: string, user?: User | null) => {
     delete serialized.feedback_verdicts
 
     // const feedbackVerdicts = await loadFeedbackVerdictsForSession(note.id)
-       const feedbackVerdicts = await loadFeedbackVerdictsForSession(note.sessionId)
-
+    const feedbackVerdicts = await loadFeedbackVerdictsForSession(note.sessionId)
 
     const noteWithCount = {
       ...serialized,
